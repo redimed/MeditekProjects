77380c8b6cb7365521ff399fd3b42b7eb861b6a7
kinhkyo <kinhkyo@gmail.com>
2015-09-23 16:42:11 +0700

    Commit Telehealth Patient GUI

---
+++ iOS/Telehealth/Patient/Telehealth/Telehealth/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  Telehealth
+//
+//  Created by Giap Vo Duc on 9/22/15.
+//  Copyright © 2015 Giap Vo Duc. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+ 
+
+}

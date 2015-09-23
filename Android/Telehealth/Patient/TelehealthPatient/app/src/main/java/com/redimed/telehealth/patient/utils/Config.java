f4b7340ed97f7e97e137b63f4b634941f10c158a
Luan Nguyen <luannguyen141194@gmail.com>
2015-09-23 16:03:55 +0700

    commit

---
+++ Android/Telehealth/Patient/TelehealthPatient/app/src/main/java/com/redimed/telehealth/patient/utils/Config.java
@@ -0,0 +1,9 @@
+package com.redimed.telehealth.patient.utils;
+
+/**
+ * Created by luann on 9/23/2015.
+ */
+public class Config {
+    public static final String apiURL = "http://192.168.1.130:3000";
+    public static final String socketURL = "http://192.168.1.130:4000";
+}
---
+++ Android/TelehealthServer/config/connections.js
@@ -0,0 +1,17 @@
+module.exports.connections = {
+  localDiskDb: {
+    adapter: 'sails-disk'
+  },
+
+  mysql: {
+      module: 'sails-mysql',
+      host: '192.168.1.2',
+      port: 3306,
+      user: 'meditek_db',
+      password: 'meditekdb123456',
+      database: 'Redimed',
+      charset: 'utf8',
+      collation: 'utf8-general_ci'
+  }
+
+};

1e14baeac10592a02118a3316eb23d5b660c4209
Luan Nguyen <luannguyen141194@gmail.com>
2015-09-24 16:38:26 +0700

    commit

--- Android/TelehealthServer/config/connections.js
+++ Android/TelehealthServer/config/connections.js
@@ -3,12 +3,23 @@ module.exports.connections = {
     adapter: 'sails-disk'
   },
 
+  // mysql: {
+  //     dialect: 'mysql',
+  //     host: '192.168.1.2',
+  //     port: 3306,
+  //     user: 'meditek_db',
+  //     password: 'meditekdb123456',
+  //     database: 'Redimed',
+  //     charset: 'utf8',
+  //     collation: 'utf8-general_ci'
+  // },
+
   mysql: {
-      module: 'sails-mysql',
-      host: '192.168.1.2',
+      dialect: 'mysql',
+      host: 'localhost',
       port: 3306,
-      user: 'meditek_db',
-      password: 'meditekdb123456',
+      user: 'root',
+      password: 'root',
       database: 'Redimed',
       charset: 'utf8',
       collation: 'utf8-general_ci'

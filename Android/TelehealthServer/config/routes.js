efdf74a48f4d6aef7afb78b3b0ced6dc0b0fb055
Luan Nguyen <luannguyen141194@gmail.com>
2015-09-25 17:04:42 +0700

    Update Telehealth Server Code

--- Android/TelehealthServer/config/routes.js
+++ Android/TelehealthServer/config/routes.js
@@ -1,28 +1,24 @@
 module.exports.routes = {
-	//=================Telehealth User Routes======================
-	'GET /test': {
-		controller: 'TelehealthController',
-		action: 'test'
-	},
-
-  	'POST /telehealth/user/requestActivationCode': {
-    	controller: 'TelehealthController',
-    	action: 'RequestActivationCode'
-  	},
-
-  	'POST /telehealth/user/verifyActivationCode': {
-  		controller: 'TelehealthController',
-  		action: 'VerifyActivationCode'
-  	},
-
-  	'POST /telehealth/user/login': {
-  		controller: 'TelehealthController',
-  		action: 'TelehealthLogin'
-  	},
-
-  	//================Telehealth Socket Routes==========================
-  	'/telehealth/socket/joinRoom': {
-  		controller: 'SocketController',
-  		action: 'JoinRoom'
-  	}
+    //=================Telehealth User Routes======================
+    'POST /telehealth/user/updateToken': {
+        controller: 'TelehealthController',
+        action: 'UpdateDeviceToken'
+    },
+    'POST /telehealth/user/requestActivationCode': {
+        controller: 'TelehealthController',
+        action: 'RequestActivationCode'
+    },
+    'POST /telehealth/user/verifyActivationCode': {
+        controller: 'TelehealthController',
+        action: 'VerifyActivationCode'
+    },
+    'POST /telehealth/user/login': {
+        controller: 'TelehealthController',
+        action: 'TelehealthLogin'
+    },
+    //================Telehealth Socket Routes==========================
+    '/telehealth/socket/joinRoom': {
+        controller: 'SocketController',
+        action: 'JoinRoom'
+    }
 };

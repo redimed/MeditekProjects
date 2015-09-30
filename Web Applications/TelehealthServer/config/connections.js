688c4feefee097058647c3abf983edf50d1aa5f0
Luan Nguyen <luannguyen141194@gmail.com>
2015-09-30 11:43:55 +0700

    Change Connection Settings Telehealth Server

--- Web Applications/TelehealthServer/config/connections.js
+++ Web Applications/TelehealthServer/config/connections.js
@@ -3,26 +3,26 @@ module.exports.connections = {
     adapter: 'sails-disk'
   },
 
-  // mysql: {
-  //     dialect: 'mysql',
-  //     host: '192.168.1.2',
-  //     port: 3306,
-  //     user: 'meditek_db',
-  //     password: 'meditekdb123456',
-  //     database: 'Redimed',
-  //     charset: 'utf8',
-  //     collation: 'utf8-general_ci'
-  // },
-
   mysql: {
       dialect: 'mysql',
-      host: 'localhost',
+      host: '192.168.1.2',
       port: 3306,
-      user: 'root',
-      password: 'root',
+      user: 'meditek_db',
+      password: 'meditekdb123456',
       database: 'Redimed',
       charset: 'utf8',
       collation: 'utf8-general_ci'
-  }
+  },
+
+  // mysql: {
+  //     dialect: 'mysql',
+  //     host: 'localhost',
+  //     port: 3306,
+  //     user: 'root',
+  //     password: 'root',
+  //     database: 'Redimed',
+  //     charset: 'utf8',
+  //     collation: 'utf8-general_ci'
+  // }
 
 };

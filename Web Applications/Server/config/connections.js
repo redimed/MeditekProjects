06f8dd903b669149183cd6f5285c053263b6c8ae
thanh1101681 <thanh1101681@gmail.com>
2015-10-08 13:51:29 +0700

    change port core

--- Web Applications/Server/config/connections.js
+++ Web Applications/Server/config/connections.js
@@ -5,25 +5,25 @@ module.exports.connections = {
 
 
     mysql: {
-        // adapter: 'sails-mysql',
-        // port: 3306,
-        // user: 'meditek_db',
-        // password: 'meditekdb123456',
-        // database: 'Redimed',
-        // charset: 'utf8',
-        // collation: 'utf8-general_ci',
-        // options: {
-        //     host: '192.168.1.2'
-        // }
         adapter: 'sails-mysql',
         port: 3306,
-        user: 'root',
-        password: 'root',
+        user: 'meditek_db',
+        password: 'meditekdb123456',
         database: 'Redimed',
         charset: 'utf8',
         collation: 'utf8-general_ci',
         options: {
             host: 'localhost'
         }
+        // adapter: 'sails-mysql',
+        // port: 3306,
+        // user: 'root',
+        // password: 'root',
+        // database: 'Redimed',
+        // charset: 'utf8',
+        // collation: 'utf8-general_ci',
+        // options: {
+        //     host: 'localhost'
+        // }
     },
 };

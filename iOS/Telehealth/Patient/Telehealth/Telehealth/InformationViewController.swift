0b16f9839f4bdd5269b7e9baf95d3554167c3bc2
kinhkyo <kinhkyo@gmail.com>
2015-10-09 16:55:05 +0700

    Update New version Patient App

---
+++ iOS/Telehealth/Patient/Telehealth/Telehealth/InformationViewController.swift
@@ -0,0 +1,98 @@
+//
+//  InformationViewController.swift
+//  Telehealth
+//
+//  Created by Giap Vo Duc on 9/23/15.
+//  Copyright © 2015 Giap Vo Duc. All rights reserved.
+//
+
+import UIKit
+
+class InformationViewController: UIViewController {
+    let InformationPatient = VerifyPhoneNumberController()
+    var uid = String()
+    
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var middleNameLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var dobLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var suburbLabel: UILabel!
+    @IBOutlet weak var postCodeLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var siteLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var homePhoneLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.showLoading()
+        if let uuid = defaults.valueForKey("uid") as? String {
+            uid = uuid
+            
+        }
+       
+        InformationPatient.getInformationPatientByUUID(uid){
+            response in
+            
+            if response["message"] == "success" {
+                self.view.hideLoading()
+                let jsonInformation = response["data"][0]
+                self.firstNameLabel.text = jsonInformation["FirstName"].string
+                self.middleNameLabel.text = jsonInformation["MiddleName"].string
+                self.lastNameLabel.text = jsonInformation["LastName"].string
+                self.dobLabel.text = (jsonInformation["DOB"].string)?.toDateTime()
+                self.suburbLabel.text = jsonInformation["Suburb"].string
+                self.postCodeLabel.text = jsonInformation["Postcode"].string
+                self.countryLabel.text = jsonInformation["CountryID"].string
+                self.siteLabel.text = jsonInformation["siteLabel"].string
+                self.addressLabel.text = jsonInformation["Address"].string
+                self.emailLabel.text = jsonInformation["Email"].string
+                self.homePhoneLabel.text = jsonInformation["HomePhoneNumber"].string
+                
+                
+                
+                
+                
+            }else {
+                self.view.hideLoading()
+                if response["TimeOut"] ==  "Request Time Out" {
+                    self.alertMessage("Error", message: "Request Time Out")
+                }else {
+                    let message : String = String(response["message"]["message"])
+                    self.alertMessage("Error", message: message)
+                }
+                
+            }
+//            print(response)
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
+    }
+    //Giap: Show alert message
+    func alertMessage(title : String,message : String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            
+        }
+        alertController.addAction(OKAction)
+        
+        self.presentViewController(alertController, animated: true) {
+            
+        }
+    }
+
+    
+    
+}
+
+
---
+++ iOS/Telehealth/Patient/Telehealth/TelehealthTests/TelehealthTests.swift
@@ -0,0 +1,36 @@
+//
+//  TelehealthTests.swift
+//  TelehealthTests
+//
+//  Created by Giap Vo Duc on 9/21/15.
+//  Copyright © 2015 Giap Vo Duc. All rights reserved.
+//
+
+import XCTest
+@testable import Telehealth
+
+class TelehealthTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

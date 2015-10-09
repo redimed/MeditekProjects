88c166381f72b780ba1c8bd1bc51838d60498fa7
kinhkyo <kinhkyo@gmail.com>
2015-10-09 10:54:10 +0700

    Commit Patient

---
+++ iOS/Telehealth/Patient/Telehealth/Telehealth/Telehealth-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Telehealth-Bridging-Header.h
+//  Telehealth
+//
+//  Created by Giap Vo Duc on 9/29/15.
+//  Copyright © 2015 Giap Vo Duc. All rights reserved.
+//
+
+#ifndef Telehealth_Bridging_Header_h
+#define Telehealth_Bridging_Header_h
+
+#import <OpenTok/OpenTok.h>
+
+
+#endif /* Telehealth_Bridging_Header_h */

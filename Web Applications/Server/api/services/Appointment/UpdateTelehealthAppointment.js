a67b202403df026192eebfdb2de7df7e19786b21
Huy Nguyễn <nnh1012@gmail.com>
2015-10-12 08:37:29 +0700

    update

--- Web Applications/Server/api/services/Appointment/UpdateTelehealthAppointment.js
+++ Web Applications/Server/api/services/Appointment/UpdateTelehealthAppointment.js
@@ -1,101 +1,107 @@
-module.exports = function(data) {
-    var $q = require('q');
-    return sequelize.transaction()
-        .then(function(t) {
-            var defer = $q.defer();
-            var dataAppointment = Services.GetDataAppointment.Appointment(data);
-            //update Appointment
-            Appointment.update(dataAppointment, {
-                    where: {
-                        UID: data.UID
-                    }
-                }, {
-                    transaction: t
-                })
-                .then(function(appointmentUpdated) {
-                    var telehealthAppointment = data.TelehealthAppointment;
-                    if (HelperService.CheckExistData(telehealthAppointment)) {
-                        //update telehealth appointment
-                        var dataTelehealthAppointment =
-                            Services.GetDataAppointment.TelehealthAppointment(telehealthAppointment);
-                        return TelehealthAppointment.update(dataTelehealthAppointment, {
-                            where: {
-                                UID: telehealthAppointment.UID
+    module.exports = function(data) {
+        var $q = require('q');
+        return sequelize.transaction()
+            .then(function(t) {
+                var defer = $q.defer();
+                var dataAppointment = Services.GetDataAppointment.Appointment(data);
+                //update Appointment
+                Appointment.update(dataAppointment, {
+                        where: {
+                            UID: data.UID
+                        }
+                    }, {
+                        transaction: t
+                    })
+                    .then(function(appointmentUpdated) {
+                            var telehealthAppointment = data.TelehealthAppointment;
+                            if (HelperService.CheckExistData(telehealthAppointment)) {
+                                var dataTelehealthAppointment =
+                                    Services.GetDataAppointment.TelehealthAppointment(telehealthAppointment);
+                                //update TelehealthAppointment
+                                return TelehealthAppointment.update(dataTelehealthAppointment, {
+                                    where: {
+                                        UID: telehealthAppointment.UID
+                                    }
+                                });
+                            } else {
+                                defer.reject({
+                                    transaction: t,
+                                    error: new Error('failed')
+                                });
                             }
-                        }, {
-                            transaction: t
-                        });
-                    } else {
+                        },
+                        function(err) {
+                            defer.reject({
+                                transaction: t,
+                                error: err
+                            });
+                        })
+                    .then(function(telehealthAppointmentUpadted) {
+                        var examinationRequired = data.TelehealthAppointment.ExaminationRequired;
+                        if (HelperService.CheckExistData(examinationRequired)) {
+                            var dataExaminationRequired =
+                                Services.GetDataAppointment.ExaminationRequired(examinationRequired);
+                            //update ExaminationRequired
+                            return ExaminationRequired.update(dataExaminationRequired, {
+                                where: {
+                                    UID: examinationRequired.UID
+                                }
+                            }, {
+                                transaction: t
+                            });
+                        }
+                    }, function(err) {
                         defer.reject({
                             transaction: t,
                             error: err
                         });
-                    }
-                }, function(err) {
-                    defer.reject({
-                        transaction: t,
-                        error: err
-                    });
-                })
-                .then(function(telehealthAppointmentUpdated) {
-                    var examinationRequired = data.TelehealthAppointment.ExaminationRequired;
-                    if (HelperService.CheckExistData(examinationRequired)) {
-                        return ExaminationRequired.update(examinationRequired, {
-                            where: {
-                                UID: examinationRequired.UID
-                            }
-                        }, {
-                            transaction: t
-                        });
-                    } else {
+                    })
+                    .then(function(examinationRequiredUpdate) {
+                        var preferedPlasticSurgeon = data.TelehealthAppointment.PreferedPlasticSurgeon;
+                        if (HelperService.CheckExistData(preferedPlasticSurgeon)) {
+                            var dataPreferedPlasticSurgeon =
+                                Services.GetDataAppointment.PreferedPlasticSurgeon(preferedPlasticSurgeon);
+                            //update PreferedPlasticSurgeon
+                            return PreferedPlasticSurgeon.update(dataPreferedPlasticSurgeon, {
+                                where: {
+                                    UID: preferedPlasticSurgeon.UID
+                                }
+                            }, {
+                                transaction: t
+                            });
+                        }
+                    }, function(err) {
                         defer.reject({
                             transaction: t,
                             error: err
                         });
-                    }
-                })
-                .then(function(examinationRequiredUpdated) {
-                    var preferedPlasticSurgeon = data.TelehealthAppointment.PreferedPlasticSurgeon;
-                    if (HelperService.CheckExistData(preferedPlasticSurgeon)) {
-                        var dataPreferedPlasticSurgeon =
-                            Services.GetDataAppointment.PreferedPlasticSurgeon(preferedPlasticSurgeon);
-                        return PreferedPlasticSurgeon.update(dataPreferedPlasticSurgeon, {
-                            where: {
-                                UID: preferedPlasticSurgeon.UID
-                            }
-                        }, {
-                            transaction: t
-                        });
-                    } else {
-                        defer.reject({
-                            transaction: t,
-                            error: err
-                        });
-                    }
-                })
-                .then(function(preferedPlasticSurgeonUpdated) {
-                    var clinicalDetails = data.TelehealthAppointment.ClinicalDetails;
-                    console.log(clinicalDetails);
-                    if (HelperService.CheckExistData(clinicalDetails)) {
-                        //update mutiple row
+                    })
+                    .then(function(preferedPlasticSurgeonUpdated) {
+                        var dataClinicalDetails = data.TelehealthAppointment.ClinicalDetails;
+                        if (HelperService.CheckExistData(dataClinicalDetails) &&
+                            _.isArray(dataClinicalDetails)) {
+                            return sequelize.Promise.each(dataClinicalDetails, function(dataClinicalDetail) {
+                                return ClinicalDetail.update(dataClinicalDetail, {
+                                    where: {
+                                        UID: dataClinicalDetail.UID
+                                    }
+                                }, {
+                                    transaction: t
+                                });
+                            });
+                        }
+                    })
+                    .then(function(clinicalDetailsUpdated) {
                         defer.resolve({
                             transaction: t,
                             status: 'success'
                         });
-                    } else {
+                    }, function(err) {
                         defer.reject({
                             transaction: t,
-                            error: 'failed'
+                            error: err
                         });
-                    }
-                })
-                .then(function(clinicalDetailUpdated) {
-                    defer.resolve({
-                        transaction: t,
-                        status: 'success'
                     });
-                });
-
-            return defer.promise;
-        });
-};
+                return defer.promise;
+            });
+    };
--- Web Applications/UrgentRequestServer/api/services/MessageQueueService.js
+++ Web Applications/UrgentRequestServer/api/services/MessageQueueService.js
@@ -30,16 +30,17 @@ module.exports = {
                 .spread(function(UR) {
                     //check tried and confirmed urgent request
                     if (UR.tried < 3 && UR.status === 'pending') {
+                        var subjectEmail = '[Testing] -[' + UR.urgentRequestType + '] - [' + (UR.tried == 1 ? '2nd' : '3rd') + '] - [' +
+                            Services.moment(UR.requestDate).format('DD/MM/YYYY HH:mm:ss') +
+                            '] - [' + UR.firstName + ' ' +
+                            UR.lastName + '] - [' + UR.phoneNumber + ']';
                         var emailInfo = {
-                            from: 'Health Screenings <HealthScreenings@redimed.com.au>',
+                            from: 'Redimed UrgentCare <HealthScreenings@redimed.com.au>',
                             email: 'HealthScreenings@redimed.com.au',
-                            subject: '[Testing] -[' + UR.urgentRequestType + '] - [' + (UR.tried === 1 ? '2nd' : '3rd') + '] - [' +
-                                Services.moment(UR.requestDate).format('DD/MM/YYYY HH:mm:ss') +
-                                '] - [' + UR.lastName + ' ' +
-                                UR.firstName + '] - [' + UR.phoneNumber + ']',
+                            subject: subjectEmail,
                             confirmed: APIService.UrgentCareConfirmURL + '/' + UR.UID,
                             urgentRequestType: UR.urgentRequestType,
-                            patientName: UR.lastName + ' ' + UR.firstName,
+                            patientName: UR.firstName + ' ' + UR.lastName,
                             requestDate: Services.moment(UR.requestDate).format('DD/MM/YYYY HH:mm:ss'),
                             phoneNumber: UR.phoneNumber,
                             companyName: UR.companyName,

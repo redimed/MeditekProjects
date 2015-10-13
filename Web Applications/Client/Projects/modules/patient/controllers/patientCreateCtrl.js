4531029d21a1f235a287c96a6c41a1de5a885ecb
giangvo1125 <giangvo2511@gmail.com>
2015-10-13 08:09:57 +0700

    update patient guide v1.2

--- Web Applications/Client/Projects/modules/patient/controllers/patientCreateCtrl.js
+++ Web Applications/Client/Projects/modules/patient/controllers/patientCreateCtrl.js
@@ -1,7 +1,8 @@
 var app = angular.module('app.authentication.patient.create.controller', [
 ]);
 
-app.controller('patientCreateCtrl', function($scope, $modal){
+app.controller('patientCreateCtrl', function($scope, $modal, $state){
 	console.log('patientCreateCtrl');
+	console.log($scope.data);
 	//$state.go('app.authentication.patient.confirm');
 });

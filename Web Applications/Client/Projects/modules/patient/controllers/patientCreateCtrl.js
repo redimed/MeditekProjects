ae0482277de4998410d42025d3480a5f71832eeb
giangvo1125 <giangvo2511@gmail.com>
2015-10-14 10:54:57 +0700

    update PatientGuideClient

--- Web Applications/Client/Projects/modules/patient/controllers/patientCreateCtrl.js
+++ Web Applications/Client/Projects/modules/patient/controllers/patientCreateCtrl.js
@@ -4,15 +4,18 @@ var app = angular.module('app.authentication.patient.create.controller', [
 app.controller('patientCreateCtrl', function($scope, $modal, $state, PatientService, toastr){
 	console.log('patientCreateCtrl');
 	$scope.er={};
-	$scope.myStyle= {};
 	$scope.isShowNext=false;
+	$scope.isBlockStep1 =false;
 	$scope.Back = function() {
 		$scope.isShowNext=false;
 		$scope.isShowCreate=false;
 	};
 
 	$scope.checkPhone = function(data) {
-		if(data.PhoneNumber!=undefined && data.PhoneNumber!=null && data.PhoneNumber!='') {
+		return $scope.validateCheckPhone(data)
+		.then(function(success){
+			$scope.er ='';
+			$scope.isBlockStep1=true;
 			PatientService.checkPatient(data)
 			.then(function(result){
 				if(result!=undefined && result!=null && result!='' && result.length!=0){
@@ -33,21 +36,22 @@ app.controller('patientCreateCtrl', function($scope, $modal, $state, PatientServ
 				toastr.error("Please input correct information","ERROR");
 				console.log(err.data.message.ErrorsList);
 				for(var i = 0; i < err.data.message.ErrorsList.length;i++){
-					if(err.data.message.ErrorsList[i].field=="FirstName"){
-						$scope.er.FirstName = {'border': '2px solid #DCA7B0'};
-					}
-					if(err.data.message.ErrorsList[i].field=="LastName"){
-						$scope.er.LastName = {'border': '2px solid #DCA7B0'};
-					}
-					if(err.data.message.ErrorsList[i].field=="PhoneNumber"){
-						$scope.er.PhoneNumber = {'border': '2px solid #DCA7B0'};
-					}
+					$scope.er[err.data.message.ErrorsList[i].field] = {'border': '2px solid #DCA7B0'};
 				}
 			});
-		}
-		else {
-			toastr.error("Please input Phone Number","ERROR");
-		}
+		},function (err){
+			console.log(err);
+			toastr.error("Please check data again.","ERROR");
+			$scope.er ={};
+			for(var i = 0; i < err.length; i++){
+				$scope.er[err[i].field] ={'border': '2px solid #DCA7B0'};
+			}
+		});
+		
+	};
+
+	$scope.Cancel = function() {
+		$state.go('authentication.patient.list');
 	}
 	//$state.go('app.authentication.patient.confirm');
 });

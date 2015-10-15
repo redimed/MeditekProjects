2acfca667fe199b896d61bf4e6c7c05eabb18164
Tan Nguyen <tannv.dts@gmail.com>
2015-10-15 14:44:08 +0700

    update common service

--- Web Applications/Client/Projects/common/services/CommonService.js
+++ Web Applications/Client/Projects/common/services/CommonService.js
@@ -1,18 +1,17 @@
 angular.module("app.common",[])
-
-.config(function($provide){// provider-injector
-	// This is an example of config block.
-	// You can have as many of these as you want.
-	// You can only inject Providers (not instances)
-	// into config blocks.
-	$provide.factory('CommonService',function(){
-		var commonService={};
-		return commonService;
-	});
-})
-.run(function($rootScope, $window, $state){// instance-injector
-	// This is an example of a run block.
-	// You can have as many of these as you want.
-	// You can only inject instances (not Providers)
-	// into run blocks
+.factory("CommonService",function(){
+	var commonService={};
+	//FUNCTION MẪU
+	commonService.getTitles=function()
+	{
+		var list=[
+			{id:1, name:'Mr'},
+			{id:2, name:'Mrs'},
+			{id:3, name:'Ms'},
+			{id:4, name:'Dr'}
+		];
+		return list; 
+	}
+	
+	return commonService;
 })

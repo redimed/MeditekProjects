3369b719b834cf9b9307b3c4ed4c7e0194b65b98
idquangtien <tien801238@gmail.com>
2015-10-15 08:45:51 +0700

    update appointment

--- Web Applications/Client/Projects/modules/appointment/controllers/appointmentRequestCtrl.js
+++ Web Applications/Client/Projects/modules/appointment/controllers/appointmentRequestCtrl.js
@@ -2,40 +2,54 @@ var app = angular.module('app.authentication.appointment.request.controller',[])
 
 app.controller('appointmentRequestCtrl', function($scope,$cookies){
 
-	$scope.requestInfo = {
-		RequestDate:null,
-		TelehealthAppointment : {
-			RefDurationOfReferal : null,
-			PatientAppointment : {
-				FirstName : null,
-				MiddleName : null,
-				LastName : null,
-				DOB : null,
-				Email : null,
-				PhoneNumber : null,
-				Address : null,
-				Suburb : null,
-				Postcode : null,
-				HomePhoneNumber : null
-			},
-			ExaminationRequired : {
-				Private : null,
-				Public : null,
-				DVA : null,
-				WorkersComp : null,
-				MVIT : null
-			},
-			PreferedPlasticSurgeon : {
-				Name : null
-			},
-			ClinicalDetails : [{
-				Section:null
-			}]
+	// $scope.requestInfo = {
+	// 	RequestDate:null,
+	// 	TelehealthAppointment : {
+	// 		RefDurationOfReferal : null,
+	// 		PatientAppointment : {
+	// 			FirstName : null,
+	// 			MiddleName : null,
+	// 			LastName : null,
+	// 			DOB : null,
+	// 			Email : null,
+	// 			PhoneNumber : null,
+	// 			Address : null,
+	// 			Suburb : null,
+	// 			Postcode : null,
+	// 			HomePhoneNumber : null
+	// 		},
+	// 		ExaminationRequired : {
+	// 			Private : null,
+	// 			Public : null,
+	// 			DVA : null,
+	// 			WorkersComp : null,
+	// 			MVIT : null
+	// 		},
+	// 		PreferedPlasticSurgeon : {
+	// 			Name : null
+	// 		},
+	// 		ClinicalDetails : [{
+	// 			Section:null
+	// 		}]
+	// 	}
+
+
+	// }
+	$scope.checkElectiveOther = false;
+	$scope.checkLacerationsOther = false;
+	$scope.checkPNSOther = false;
+
+	$scope.checkboxOther = function(){
+		if($scope.checkLacerationsOther == false){
+			$scope.txtLacerationsOther = null;
 		}
-
-
-	}
-
+		if($scope.checkElectiveOther == false){
+			$scope.txtElectiveOther = null;
+		}
+		if($scope.checkPNSOther == false){
+			$scope.txtPNSOther = null;
+		}
+	};
 
 	console.log($cookies.getObject('userInfo'));
 	$scope.sendRequestAppointment = function() {

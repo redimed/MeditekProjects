1a947249b868cee06242b88daa510dca53a80d17
khuongphan <khuong@Khuongs-MacBook-Pro.local>
2015-10-16 15:44:45 +0700

    no message

--- Web Applications/FileUpload/resources/views/UploadFile.blade.php
+++ Web Applications/FileUpload/resources/views/UploadFile.blade.php
@@ -68,7 +68,7 @@ html, body {
             <div class="content">
                 <div>Meditek Upload File</div>
                 <!-- <form class="form" method="post" action="../api/UploadFile" enctype="multipart/form-data"> -->
-                <form class="form" method="post" action="../api/UploadFileTelehealthPatient" enctype="multipart/form-data">
+                <form class="form" method="post" action="../api/UploadSingleFileTelehealth" enctype="multipart/form-data">
                     <div><input value='6a316ebf-0ea8-4365-ac95-8b9ab340f2cf' type='text' name='userUID' /></div>
                     <div><input value='c2af34c7-1365-4406-b087-861b74fa5f79' type='test' name='patientUID' /></div>
                     <div>
@@ -79,8 +79,9 @@ html, body {
                         <option value="Signature">Signature</option>
                     </select>
                     </div>
-                    <div><input type="file" multiple="multiple" name="uploadFiles[]" id="uploadFiles"/></div>
-
+<!--                     <div><input type="file" multiple="multiple" name="uploadFiles[]" id="uploadFiles"/></div>
+ -->
+                    <div><input type="file" name="uploadFile" id="uploadFiles"/></div>
                     <span>&nbsp;</span>
                     <div><input class="form-input" type="submit" value="Upload Files" />
                     </div>

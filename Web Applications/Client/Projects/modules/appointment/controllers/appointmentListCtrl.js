b4dd0e816ef19c803a1e7b47cb06e0e4ca8f9aa1
Duc Manh <ngaokiems1@yahoo.com>
2015-10-16 13:51:34 +0700

    appointment detail

--- Web Applications/Client/Projects/modules/appointment/controllers/appointmentListCtrl.js
+++ Web Applications/Client/Projects/modules/appointment/controllers/appointmentListCtrl.js
@@ -2,20 +2,6 @@ var app = angular.module('app.authentication.appointment.list.controller', [
 	'app.authentication.appointment.list.modal.controller'
 ]);
 
-app.controller('appointmentListCtrl', function($scope, $modal, $state,AppointmentService){
+app.controller('appointmentListCtrl', function(){
 
-	$scope.openAppointmentModal = function(){
-		var modalInstance = $modal.open({
-			animation: true,
-			templateUrl:'modules/appointment/views/appointmentListModal.html',
-			controller: 'appointmentListModalCtrl',
-			windowClass : 'app-modal-window',
-			//size: 'lg',
-			resolve: {
-				getid: function(){
-					return true;
-				}
-			}
-		});
-	};
 });

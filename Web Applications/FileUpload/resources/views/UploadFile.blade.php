8bb8178c60b92692709cd6423b8f462d290f6490
khuongphan <khuong@Khuongs-MacBook-Pro.local>
2015-10-16 10:27:47 +0700

    FileUpload: Single Upload File Telehealth :  /api/UploadSingleFileTelehealth

--- Web Applications/FileUpload/resources/views/UploadFile.blade.php
+++ Web Applications/FileUpload/resources/views/UploadFile.blade.php
@@ -66,17 +66,24 @@ html, body {
     <body>
         <div class="container">
             <div class="content">
-                <form class="form" method="post" action="../api/UploadFile" enctype="multipart/form-data">
+                <div>Meditek Upload File</div>
+                <!-- <form class="form" method="post" action="../api/UploadFile" enctype="multipart/form-data"> -->
+                <form class="form" method="post" action="../api/UploadFileTelehealthPatient" enctype="multipart/form-data">
+                    <div><input value='6a316ebf-0ea8-4365-ac95-8b9ab340f2cf' type='text' name='userUID' /></div>
+                    <div><input value='c2af34c7-1365-4406-b087-861b74fa5f79' type='test' name='patientUID' /></div>
+                    <div>
                     <select name="fileType" >
                         <option value="MedicalImage">Medical Image</option>
                         <option value="MedicalDocument">Medical Document</option>
                         <option value="ProfileImage">Profile Image</option>
                         <option value="Signature">Signature</option>
                     </select>
-                    <input type="file" multiple="multiple" name="uploadFiles[]" id="uploadFiles"/>
+                    </div>
+                    <div><input type="file" multiple="multiple" name="uploadFiles[]" id="uploadFiles"/></div>
 
                     <span>&nbsp;</span>
-                    <input class="form-input" type="submit" value="Upload Files" />
+                    <div><input class="form-input" type="submit" value="Upload Files" />
+                    </div>
                 </form>
             </div>
         </div>

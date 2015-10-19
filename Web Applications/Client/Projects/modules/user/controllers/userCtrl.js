fcc81baf446ff96f359d6bed691e8a5873eafa08
idquangtien <tien801238@gmail.com>
2015-10-19 17:10:54 +0700

    create module user

---
+++ Web Applications/Client/Projects/modules/user/controllers/userCtrl.js
@@ -0,0 +1,8 @@
+var app = angular.module('app.authentication.user.controller',[
+	'app.authentication.user.list.controller',
+	'app.authentication.user.Profile.controller',
+]);
+
+app.controller('userCtrl', function(){
+	console.log('userCtrl');
+});

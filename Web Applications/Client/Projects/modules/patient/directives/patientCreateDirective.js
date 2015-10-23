7a6dd6a5010474882064e919bb981f19d97a0c4b
giangvo1125 <giangvo2511@gmail.com>
2015-10-23 20:40:08 +0700

    updated

--- Web Applications/Client/Projects/modules/patient/directives/patientCreateDirective.js
+++ Web Applications/Client/Projects/modules/patient/directives/patientCreateDirective.js
@@ -1,5 +1,5 @@
 var app = angular.module('app.authentication.patient.create.directive',[]);
-app.directive('patientCreate',function(toastr, PatientService, $state, $timeout){
+app.directive('patientCreate',function(toastr, PatientService, $state, $timeout, FileUploader, $cookies){
 	return {
 		restrict: "EA",
 		link: function(scope, elem, attrs){
@@ -95,7 +95,73 @@ app.directive('patientCreate',function(toastr, PatientService, $state, $timeout)
 						scope.ermsg[err[i].field] = err[i].message;
 					}
 				});
-			}
+			};
+
+			scope.SendRequestUploadFile = function () {
+		        for (var i = 0; i < uploader.queue.length; i++) {
+		            console.log(' uploader.queue', uploader.queue);
+		            var item = uploader.queue[i];
+		            item.formData[i] = {};
+		            item.formData[i].userUID = $cookies.getObject('userInfo').UID;
+		            item.formData[i].fileType = 'MedicalImage';
+		        };
+		        uploader.uploadAll();
+		    }
+
+
+		    var uploader = scope.uploader = new FileUploader({
+		        url: 'http://telehealthvietnam.com.vn:3005/api/uploadFile',
+		        alias: 'uploadFile'
+		    });
+
+		    // FILTERS
+
+		    uploader.filters.push({
+		        name: 'customFilter',
+		        fn: function(item /*{File|FileLikeObject}*/ , options) {
+		            return this.queue.length < 10;
+		        }
+		    });
+
+		    // CALLBACKS
+
+		    uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/ , filter, options) {
+		        console.info('onWhenAddingFileFailed', item, filter, options);
+		    };
+		    uploader.onAfterAddingFile = function(fileItem) {
+		        console.info('onAfterAddingFile', fileItem);
+		    };
+		    uploader.onAfterAddingAll = function(addedFileItems) {
+		        console.info('onAfterAddingAll', addedFileItems);
+		    };
+		    uploader.onBeforeUploadItem = function(item) {
+		        console.info('onBeforeUploadItem', item);
+		    };
+		    uploader.onProgressItem = function(fileItem, progress) {
+		        console.info('onProgressItem', fileItem, progress);
+		    };
+		    uploader.onProgressAll = function(progress) {
+		        console.info('onProgressAll', progress);
+		    };
+		    uploader.onSuccessItem = function(fileItem, response, status, headers) {
+		        console.info('onSuccessItem', fileItem, response, status, headers);
+		    };
+		    uploader.onErrorItem = function(fileItem, response, status, headers) {
+		        console.info('onErrorItem', fileItem, response, status, headers);
+		    };
+		    uploader.onCancelItem = function(fileItem, response, status, headers) {
+		        console.info('onCancelItem', fileItem, response, status, headers);
+		    };
+		    uploader.onCompleteItem = function(fileItem, response, status, headers) {
+		        console.info('onCompleteItem', fileItem, response, status, headers);
+		        if (response.status == 'success') {
+		            scope.requestInfo.FileUploads.push({UID:response.fileUID});
+		        };
+		    };
+		    uploader.onCompleteAll = function() {
+		    	
+		    };
+
 		},
 		templateUrl:"modules/patient/directives/template/patientCreate.html"
 	}

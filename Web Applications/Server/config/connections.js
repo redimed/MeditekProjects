9c0154de7b2136472bf1525fbf6ec1f47c965a94
thanh1101681 <thanh1101681@gmail.com>
2015-10-23 20:32:54 +0700

    fix api Update

--- Web Applications/Server/config/connections.js
+++ Web Applications/Server/config/connections.js
@@ -6,7 +6,7 @@ module.exports.connections = {
         adapter: 'sails-mysql',
         port: 3306,
         user: 'meditek_db',
-        password: 'meditek123456',
+        password: 'meditekdb123456',
         // password: 'tz!@9\8$3a?=G].T',
         database: 'Redimed',
         charset: 'utf8',

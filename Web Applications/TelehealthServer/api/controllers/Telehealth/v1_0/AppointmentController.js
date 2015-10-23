45a2d430cfa5d86e0d244243e041da07526bc30e
Luan Nguyen <luannguyen141194@gmail.com>
2015-10-23 17:49:03 +0700

    Fix Telehealth Server

--- Web Applications/TelehealthServer/api/controllers/Telehealth/v1_0/AppointmentController.js
+++ Web Applications/TelehealthServer/api/controllers/Telehealth/v1_0/AppointmentController.js
@@ -6,7 +6,7 @@ module.exports = {
             return res.serverError(ErrorWrap(err));
         }
         var info = HelperService.toJson(req.body.data);
-        if (!info.fileuid || !info.apptid) {
+        if (!info.fileuid || !info.apptuid) {
             var err = new Error("Appointment.UpdateFile.Error");
             err.pushError("Invalid Params");
             return res.serverError(ErrorWrap(err));
@@ -21,13 +21,27 @@ module.exports = {
                 err.pushError("File Not Exists");
                 return res.serverError(ErrorWrap(err));
             }
-            RelAppointmentFileUpload.create({
-                FileUploadID: file.ID,
-                AppointmentID: info.apptid
-            }).then(function() {
-                res.ok({
-                    status: 'success'
-                });
+            Appointment.find({
+                where: {
+                    UID: info.apptuid
+                }
+            }).then(function(appt) {
+                if (appt) {
+                    RelAppointmentFileUpload.create({
+                        FileUploadID: file.ID,
+                        AppointmentID: appt.ID
+                    }).then(function() {
+                        res.ok({
+                            status: 'success'
+                        });
+                    }).catch(function(err) {
+                        res.serverError(ErrorWrap(err));
+                    })
+                } else {
+                    var err = new Error("Appointment.UpdateFile.Error");
+                    err.pushError("Appointment Not Exists");
+                    return res.serverError(ErrorWrap(err));
+                }
             }).catch(function(err) {
                 res.serverError(ErrorWrap(err));
             })
---
+++ Web Applications/TelehealthServer/api/models/Appointment.js
@@ -0,0 +1,151 @@
+module.exports = {
+    attributes: {
+        ID: {
+            type: Sequelize.BIGINT(20),
+            autoIncrement: true,
+            allowNull: false,
+            validate: {
+                isInt: {
+                    msg: 'Must be an integer!'
+                }
+            },
+            primaryKey: true
+        },
+        UID: {
+            type: Sequelize.STRING(255),
+            allowNull: false,
+            validate: {
+                isUUID: {
+                    args: 4,
+                    msg: 'Must be an UUID V4!'
+                }
+            }
+        },
+        SiteID: {
+            type: Sequelize.BIGINT(20),
+            allowNull: true,
+            validate: {
+                isInt: {
+                    msg: 'Must be an integer!'
+                }
+            },
+            references: {
+                model: 'Site',
+                key: 'ID'
+            }
+        },
+        FromTime: {
+            type: Sequelize.DATE,
+            allowNull: true,
+            validate: {
+                isDate: {
+                    msg: 'Invalid!'
+                }
+            }
+        },
+        ToTime: {
+            type: Sequelize.DATE,
+            allowNull: true,
+            validate: {
+                isDate: {
+                    msg: 'Invalid!'
+                }
+            }
+        },
+        RequestDate: {
+            type: Sequelize.DATE,
+            allowNull: true,
+            validate: {
+                isDate: {
+                    msg: 'Invalid!'
+                }
+            }
+        },
+        ApprovalDate: {
+            type: Sequelize.DATE,
+            allowNull: true,
+            validate: {
+                isDate: {
+                    msg: 'Invalid!'
+                }
+            }
+        },
+        Status: {
+            type: Sequelize.STRING(45),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 45],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        Enable: {
+            type: Sequelize.STRING(1),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 1],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        CreatedDate: {
+            type: Sequelize.DATE,
+            allowNull: true,
+            validate: {
+                isDate: {
+                    msg: 'Invalid!'
+                }
+            }
+        },
+        CreatedBy: {
+            type: Sequelize.BIGINT(20),
+            allowNull: true,
+            validate: {
+                isInt: {
+                    msg: 'Must be an integer!'
+                }
+            }
+        },
+        ModifiedDate: {
+            type: Sequelize.DATE,
+            allowNull: true,
+            validate: {
+                isDate: {
+                    msg: 'Invalid!'
+                }
+            }
+        },
+        ModifiedBy: {
+            type: Sequelize.BIGINT(20),
+            allowNull: true,
+            validate: {
+                isInt: {
+                    msg: 'Must be an integer!'
+                }
+            }
+        }
+    },
+    associations: function() {},
+    options: {
+        tableName: 'Appointment',
+        timestamps: false,
+        hooks: {
+            beforeCreate: function(appointment, options, callback) {
+                appointment.CreatedDate = new Date();
+                callback();
+            },
+            beforeBulkCreate: function(appointments, options, callback) {
+                appointments.forEach(function(appointment, index) {
+                    appointments[index].CreatedDate = new Date();
+                });
+                callback();
+            },
+            beforeUpdate: function(appointment, options, callback) {
+                appointment.ModifiedDate = new Date();
+                callback();
+            }
+        }
+    }
+};

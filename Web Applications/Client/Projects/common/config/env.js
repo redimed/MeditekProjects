4595f93c180aef7084990071fa259a17f4445bbc
Tan Nguyen <tannv.dts@gmail.com>
2015-10-29 11:20:31 +0700

    update env

--- Web Applications/Client/Projects/common/config/env.js
+++ Web Applications/Client/Projects/common/config/env.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 var env='testApp'; //local,meditekServer,testApp,app
-=======
-var env='local'; //local,meditekServer,testApp,app
->>>>>>> push fileDownload, update config
+
 
 var configRestBaseUrl={
 	'local':'http://localhost:3005',
@@ -11,29 +8,18 @@ var configRestBaseUrl={
 	'app':'http://apps.redimed.com.au:3005',
 };
 
-<<<<<<< HEAD
+
 if(env == "local")
-=======
-if(env=="local")
->>>>>>> push fileDownload, update config
 {
 	var _restBaseURL=configRestBaseUrl.local;
 	var _fileBaseURL=configRestBaseUrl.local;
 }
-<<<<<<< HEAD
 else if(env == 'meditekServer')
-=======
-else if(env=='meditekServer')
->>>>>>> push fileDownload, update config
 {
 	var _restBaseURL=configRestBaseUrl.meditekServer;
 	var _fileBaseURL=configRestBaseUrl.meditekServer;
 }
-<<<<<<< HEAD
 else if(env == 'testApp')
-=======
-else if(env=='testApp')
->>>>>>> push fileDownload, update config
 {
 	var _restBaseURL=configRestBaseUrl.testApp;
 	var _fileBaseURL=configRestBaseUrl.testApp;
@@ -43,11 +29,7 @@ else if (env == 'testAppLocal')
 	var _restBaseURL=configRestBaseUrl.local;
 	var _fileBaseURL=configRestBaseUrl.local;
 }
-<<<<<<< HEAD
 else if(env =='app')
-=======
-else if(env=='app')
->>>>>>> push fileDownload, update config
 {
 	var _restBaseURL=configRestBaseUrl.app;
 	var _fileBaseURL=configRestBaseUrl.app;

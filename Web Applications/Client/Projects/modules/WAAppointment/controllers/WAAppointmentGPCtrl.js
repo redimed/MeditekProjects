f81907ebcfbedf6a7b6c1e930cecc706323edf25
Duc Manh <ngaokiems1@yahoo.com>
2015-11-02 14:10:08 +0700

    update wwappointment

--- Web Applications/Client/Projects/modules/WAAppointment/controllers/WAAppointmentGPCtrl.js
+++ Web Applications/Client/Projects/modules/WAAppointment/controllers/WAAppointmentGPCtrl.js
@@ -8,6 +8,9 @@ app.controller('WAAppointmentGPCtrl', function(WAAppointmentService, $scope, $co
             PreferredPractitioner: [{
                 Speciality: ''
             }],
+            PatientAppointment : {
+
+            }
         }
 
     }
@@ -27,7 +30,49 @@ app.controller('WAAppointmentGPCtrl', function(WAAppointmentService, $scope, $co
         if ($scope.requestInfo.TelehealthAppointment.PatientAppointment.Gender === 'Other') {
             $scope.requestInfo.TelehealthAppointment.PatientAppointment.Gender = $scope.showData.GenderOther
         }
+        //console.log($scope.requestInfo)
+        var ClinicalDetailsTemp = [];
+        //console.log($scope.requestInfo.TelehealthAppointment.ClinicalDetails['Clinical__Details.Telehealth__WAAppointment.Skin__Cancer.BCC'].Value)
+        for (var key in $scope.requestInfo.TelehealthAppointment.ClinicalDetails) {
+            var newkey = key.split("__").join(" ")
+            var res = newkey.split(".");
+            console.log(res)
+            var object = {
+                Section: res[0],
+                Category: res[1],
+                Type: res[2],
+                Name: res[3],
+                Value: $scope.requestInfo.TelehealthAppointment.ClinicalDetails[key].Value
+            }
+            var isExist = false
+            
+            ClinicalDetailsTemp.forEach(function(valueTemp, keyTemp) {
+                if (valueTemp.Section == object.Section &&
+                    valueTemp.Category == object.Category &&
+                    valueTemp.Type == object.Type &&
+                    valueTemp.Name == object.Name) {
+                    isExist = true
+                }
+            })
+            if (!isExist) {
+                ClinicalDetailsTemp.push(object)
+            };
+        };
+        var countCliniDetail = 0
+        ClinicalDetailsTemp.forEach(function(value, key) {
+            if (value.Value != 'N' && value.Value != null) {
+                countCliniDetail++
+            };
+        })
+        if (countCliniDetail == 0) {
+            ClinicalDetailsTemp = []
+        }
         console.log($scope.requestInfo)
+        //$scope.requestInfo.TelehealthAppointment.ClinicalDetails = ClinicalDetailsTemp
+        // WAAppointmentService.RequestWAApointment($scope.requestInfo).then(function(response){
+        //     console.log(response)
+        //     alert('1')
+        // })
     }
     $scope.Skin_cancer_Others = false;
     $scope.info = {};
@@ -66,8 +111,24 @@ app.controller('WAAppointmentGPCtrl', function(WAAppointmentService, $scope, $co
             return this.queue.length < 10;
         }
     });
-
+    uploader.yourMethod = function() {
+        alert('12')
+    };
+    
+    $scope.ClickUploader = function(Type){ 
+        angular.element('#'+Type).click();
+        uploader.onAfterAddingFile = function(fileItem) {
+            var position = fileItem.formData.length
+            fileItem.formData.push({fileTypeClinical:Type})
+        };
+    }
     // CALLBACKS
+    $scope.ClickRemoveAll = function(Type){
+        //angular.element('#referralPresentinga').click();
+         uploader.clearQueue = function(){
+            console.log("clearQueue")
+        }
+    }
 
     uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/ , filter, options) {
         console.info('onWhenAddingFileFailed', item, filter, options);
@@ -96,6 +157,15 @@ app.controller('WAAppointmentGPCtrl', function(WAAppointmentService, $scope, $co
     uploader.onCancelItem = function(fileItem, response, status, headers) {
         console.info('onCancelItem', fileItem, response, status, headers);
     };
+    uploader.clearQueue = function(type){
+        var queueTemp = angular.copy(uploader.queue);
+        
+       for(var i = uploader.queue.length -1; i >= 0 ; i--){
+            if(uploader.queue[i].formData[0].fileTypeClinical == type){
+                uploader.queue.splice(i, 1);
+            }
+        }
+    }
     uploader.onCompleteItem = function(fileItem, response, status, headers) {
         console.info('onCompleteItem', fileItem, response, status, headers);
         if (response.status == 'success') {
--- Web Applications/Client/Projects/modules/WAAppointment/services/WAAppointmentService.js
+++
@@ -1,6 +0,0 @@
-angular.module("app.authentication.WAAppointment.services",[])
-	.factory("WAAppointmentService",function(Restangular){
-		var services = {};
-		var api = Restangular.all("api");
-		return services;
-	});

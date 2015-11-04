e68da450903edb53edfcf582e7fd45a9e822a050
idquangtien <tien801238@gmail.com>
2015-11-04 17:27:29 +0700

    update urgent care

--- Web Applications/Client/Projects/modules/home/controllers/homeListCtrl.js
+++ Web Applications/Client/Projects/modules/home/controllers/homeListCtrl.js
@@ -4,7 +4,68 @@ var app = angular.module('app.authentication.home.list.controller',[
 
 app.controller('homeListCtrl', function($scope){
 	console.log('homeListCtrl');
-	   //  $('.timepicker').timepicker({
-    //     defaultTime: false
-    // });
+
+
+        var data = [
+	        "Albania",
+	        "Andorra",
+	        "Armenia",
+	        "Austria",
+	        "Azerbaijan",
+	        "Belarus",
+	        "Belgium",
+	        "Bosnia & Herzegovina",
+	        "Bulgaria",
+	        "Croatia",
+	        "Cyprus",
+	        "Czech Republic",
+	        "Denmark",
+	        "Estonia",
+	        "Finland",
+	        "France",
+	        "Georgia",
+	        "Germany",
+	        "Greece",
+	        "Hungary",
+	        "Iceland",
+	        "Ireland",
+	        "Italy",
+	        "Kosovo",
+	        "Latvia",
+	        "Liechtenstein",
+	        "Lithuania",
+	        "Luxembourg",
+	        "Macedonia",
+	        "Malta",
+	        "Moldova",
+	        "Monaco",
+	        "Montenegro",
+	        "Netherlands",
+	        "Norway",
+	        "Poland",
+	        "Portugal",
+	        "Romania",
+	        "Russia",
+	        "San Marino",
+	        "Serbia",
+	        "Slovakia",
+	        "Slovenia",
+	        "Spain",
+	        "Sweden",
+	        "Switzerland",
+	        "Turkey",
+	        "Ukraine",
+	        "United Kingdom",
+	        "Vatican City"
+    ];
+
+    // create AutoComplete UI component
+    angular.element("#countries").kendoAutoComplete({
+	    dataSource: data,
+	    filter: "startswith",
+	    placeholder: "Select country...",
+	    separator: ", "
+    });
+
+
 });

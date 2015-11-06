404df11f00353b336e97a0c33ec25a5dec40ae6b
kinhkyo <kinhkyo@gmail.com>
2015-11-06 14:38:39 +0700

    Create WorkInjury

---
+++ iOS/UrgentCare/UrgentCare Work/UrgentCare Work/UrgentCare Work/extension.swift
@@ -0,0 +1,18 @@
+//
+//  extension.swift
+//  UrgentCare Sport
+//
+//  Created by Giap Vo Duc on 11/3/15.
+//  Copyright © 2015 Giap Vo Duc. All rights reserved.
+//
+
+import Foundation
+extension String {
+ 
+    var capitalizeFirst:String {
+        var result = self
+        result.replaceRange(startIndex...startIndex, with: String(self[startIndex]).capitalizedString)
+        return result
+    }
+ 
+}

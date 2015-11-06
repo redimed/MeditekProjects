8ddacf53b735f414ab45a85ff6dfc41ec4d6936e
QuocChien <phanquocchien.c1109g@gmail.com>
2015-11-06 14:14:26 +0700

    update wa

--- Web Applications/Client/Projects/modules/WAAppointment/controllers/WAAppointmentListDetailCtrl.js
+++ Web Applications/Client/Projects/modules/WAAppointment/controllers/WAAppointmentListDetailCtrl.js
@@ -1,8 +1,7 @@
-var app = angular.module('app.authentication.WAAppointment.list.detail.controller',[
-]);
+var app = angular.module('app.authentication.WAAppointment.list.detail.controller', []);
 
-app.controller('WAAppointmentListDetailCtrl', function($scope, $modalInstance,data){
-	$modalInstance.rendered.then(function() {
+app.controller('WAAppointmentListDetailCtrl', function($scope, $modalInstance, data, AppointmentService, toastr) {
+    $modalInstance.rendered.then(function() {
         App.initComponents(); // init core components
         App.initAjax();
         ComponentsDateTimePickers.init(); // init todo page
@@ -11,8 +10,37 @@ app.controller('WAAppointmentListDetailCtrl', function($scope, $modalInstance,da
         Portfolio.init();
         //ComponentsDropdowns.init(); // init todo page
     });
-	$scope.wainformation = data;
-	$scope.close = function(){
-		$modalInstance.close();
-	};
+    $scope.wainformation = data;
+    console.log('$scope.wainformation',$scope.wainformation);
+    $scope.info = {
+        apptStatus: WAConstant.apptStatus,
+        requestDate: moment($scope.wainformation.CreatedDate).format('DD/MM/YYYY'),
+        listDoctorTreatingPractitioner: null,
+        patientInfomation: ($scope.wainformation.Patients.length != 0) ? $scope.wainformation.Patients : $scope.wainformation.TelehealthAppointment.PatientAppointment,
+        appointmentDate: ($scope.wainformation.FromTime != null) ? moment($scope.wainformation.FromTime).utc().format('DD/MM/YYYY') : null,
+        appointmentTime: ($scope.wainformation.FromTime != null) ? moment($scope.wainformation.FromTime).utc().format('h:mm A') : null,
+        listDoctorTreatingPractitioner: null,
+        radioTest: 'Y'
+    }
+
+    $scope.loadAllDoctor = function() {
+        AppointmentService.ListDoctor().then(function(data) {
+            $scope.info.listDoctorTreatingPractitioner = data;
+        });
+    }
+
+    $scope.loadAllDoctor();
+
+    $scope.selectTreatingPractitioner = function(data) {
+        AppointmentService.getDoctorById({
+            UID: data
+        }).then(function(data) {
+            $scope.wainformation.Doctors[0] = data[0];
+            toastr.success("Select doctor successfully", "Success");
+        })
+    }
+
+    $scope.close = function() {
+        $modalInstance.close();
+    };
 });

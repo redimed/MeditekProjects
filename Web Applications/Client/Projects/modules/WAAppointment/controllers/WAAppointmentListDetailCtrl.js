6d5c9231d2b5ae9a15de1c1b1262da39734bf919
QuocChien <phanquocchien.c1109g@gmail.com>
2015-11-06 11:13:20 +0700

    update detail wa

--- Web Applications/Client/Projects/modules/WAAppointment/controllers/WAAppointmentListDetailCtrl.js
+++ Web Applications/Client/Projects/modules/WAAppointment/controllers/WAAppointmentListDetailCtrl.js
@@ -2,6 +2,15 @@ var app = angular.module('app.authentication.WAAppointment.list.detail.controlle
 ]);
 
 app.controller('WAAppointmentListDetailCtrl', function($scope, $modalInstance,data){
+	$modalInstance.rendered.then(function() {
+        App.initComponents(); // init core components
+        App.initAjax();
+        ComponentsDateTimePickers.init(); // init todo page
+        ComponentsSelect2.init(); // init todo page
+        ComponentsBootstrapSelect.init(); // init todo page
+        Portfolio.init();
+        //ComponentsDropdowns.init(); // init todo page
+    });
 	$scope.wainformation = data;
 	$scope.close = function(){
 		$modalInstance.close();
--- Web Applications/Client/Projects/modules/WAAppointment/directives/WAAppointmentDetail.js
+++ Web Applications/Client/Projects/modules/WAAppointment/directives/WAAppointmentDetail.js
@@ -1,27 +1,27 @@
 var app = angular.module('app.authentication.WAAppointment.directives.detailWAAppoint', []);
-app.directive('detailWaappoint', function(AppointmentService, toastr) {
+app.directive('detailWaappoint', function(AppointmentService, toastr, $timeout, $rootScope) {
     return {
         scope: {
             wainformation: '='
         },
         restrict: 'E',
         templateUrl: "modules/WAAppointment/directives/templates/WAAppointmentDetail.html",
-        controller: function($scope) {
+        link: function($scope) {
             console.log('wainformation', $scope.wainformation);
             $scope.info = {
                 apptStatus: WAConstant.apptStatus,
                 requestDate: moment($scope.wainformation.CreatedDate).format('DD/MM/YYYY'),
-                listDoctorTreatingPractitioner:null,
-                patientInfomation:($scope.wainformation.Patients.length != 0)?$scope.wainformation.Patients:$scope.wainformation.TelehealthAppointment.PatientAppointment,
-                appointmentDate:($scope.wainformation.FromTime != null)?moment($scope.wainformation.FromTime).utc().format('DD/MM/YYYY'):null,
-                appointmentTime:($scope.wainformation.FromTime != null)?moment($scope.wainformation.FromTime).utc().format('h:mm A'):null,
-                listDoctorTreatingPractitioner:null,
+                listDoctorTreatingPractitioner: null,
+                patientInfomation: ($scope.wainformation.Patients.length != 0) ? $scope.wainformation.Patients : $scope.wainformation.TelehealthAppointment.PatientAppointment,
+                appointmentDate: ($scope.wainformation.FromTime != null) ? moment($scope.wainformation.FromTime).utc().format('DD/MM/YYYY') : null,
+                appointmentTime: ($scope.wainformation.FromTime != null) ? moment($scope.wainformation.FromTime).utc().format('h:mm A') : null,
+                listDoctorTreatingPractitioner: null,
+                radioTest: 'Y'
             }
 
             $scope.loadAllDoctor = function() {
                 AppointmentService.ListDoctor().then(function(data) {
                     $scope.info.listDoctorTreatingPractitioner = data;
-                    console.log('$scope.info.listDoctorTreatingPractitioner',$scope.info.listDoctorTreatingPractitioner)
                 });
             }
 

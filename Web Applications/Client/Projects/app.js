c072d1f3e1fcf474d4a03876ec498604649f8e98
idquangtien <tien801238@gmail.com>
2015-11-23 10:42:31 +0700

    fix tab, not finish

--- Web Applications/Client/Projects/app.js
+++ Web Applications/Client/Projects/app.js
@@ -224,7 +224,7 @@ app
             ComponentsBootstrapSelect.init(); // init todo page
         });
         $rootScope.$on('$includeContentLoaded', function() {
-            App.initAjax();
+            //App.initAjax();
             FormWizard.init(); // form step
             ComponentsDateTimePickers.init(); // init todo page
         });

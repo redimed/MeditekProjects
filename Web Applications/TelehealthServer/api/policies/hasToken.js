4498837cbc5c69c6941b678280f2d2189f88de81
Luan Nguyen <luannguyen141194@gmail.com>
2015-12-07 11:02:57 +0700

    commit

--- Web Applications/TelehealthServer/api/policies/hasToken.js
+++ Web Applications/TelehealthServer/api/policies/hasToken.js
@@ -1,14 +1,14 @@
-var jwt = require('jsonwebtoken');
-var _ = require('lodash');
-var config = sails.config.myconf;
-var fs = require('fs');
-module.exports = function(req, res, next) {
-    if (req.isAuthenticated()) return next();
-    else {
-        var error = new Error("CheckToken");
-        error.pushError("notAuthenticated");
-        return res.unauthorize(ErrorWrap(error));
-    }
-}
-// var meditek_library=require("meditek_library");
-// module.exports=meditek_library.isAuthenticated;
+// var jwt = require('jsonwebtoken');
+// var _ = require('lodash');
+// var config = sails.config.myconf;
+// var fs = require('fs');
+// module.exports = function(req, res, next) {
+//     if (req.isAuthenticated()) return next();
+//     else {
+//         var error = new Error("CheckToken");
+//         error.pushError("notAuthenticated");
+//         return res.unauthorize(ErrorWrap(error));
+//     }
+// }
+var meditek_library=require("meditek_library");
+module.exports=meditek_library.isAuthenticated;

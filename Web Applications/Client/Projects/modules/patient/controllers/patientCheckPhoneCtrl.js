c76ac53d5eb0b2a1dd1b2153c7d4ccc321165829
giangvo1125 <giangvo1125@gmail.com>
2015-12-24 21:30:23 +0700

    update patient

--- Web Applications/Client/Projects/modules/patient/controllers/patientCheckPhoneCtrl.js
+++ Web Applications/Client/Projects/modules/patient/controllers/patientCheckPhoneCtrl.js
@@ -3,4 +3,5 @@ var app = angular.module('app.authentication.patient.checkPhone.controller', [
 
 app.controller('patientCheckPhoneCtrl', function($scope, $uibModal, PatientService, toastr, $state){
 	console.log('patientCheckPhoneCtrl');
+console.log("ahihihih");
 });

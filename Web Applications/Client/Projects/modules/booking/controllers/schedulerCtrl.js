6a553c8d49483739830563e41aa464a14c93b5eb
idquangtien <tien801238@gmail.com>
2016-01-25 13:49:01 +0700

    update scheduler

--- Web Applications/Client/Projects/modules/booking/controllers/schedulerCtrl.js
+++ Web Applications/Client/Projects/modules/booking/controllers/schedulerCtrl.js
@@ -6,7 +6,20 @@ var app = angular.module('app.authentication.booking.scheduler.controller',[
 
 app.controller('schedulerCtrl', function($scope,$timeout, $uibModal){
 	$scope.events = [
-	    {
+        { 
+            id: 'bg1', 
+            resourceId: 'a', 
+            rendering: 'background', 
+            color: 'blue',
+            start: '2016-01-07T01:00:00', 
+            end: '2016-01-07T02:30:00' 
+        }, { 
+            id: 'bg2', 
+            resourceId: 'b', 
+            rendering: 'background', 
+            start: '2016-01-07T01:00:00', 
+            end: '2016-01-07T04:30:00' 
+        }, {
             _id: '1',
 	        id: '1',
 	        resourceId: 'a',
@@ -23,6 +36,7 @@ app.controller('schedulerCtrl', function($scope,$timeout, $uibModal){
 	        end: '2016-01-07T14:00:00',
 	        title: 'event 2',
             tooltip: 'This is a cool event2',
+
 	    }, {
             _id: '3',
 	        id: '3',
@@ -80,133 +94,112 @@ app.controller('schedulerCtrl', function($scope,$timeout, $uibModal){
     };
 
     $scope.eventAfterRender = function(event, element, view){
-        $(element).attr('id', 'event_id_'+event._id);
-        $.contextMenu({
-            selector: '#event_id_'+event._id,
-            items: {
-                edit: {
-                    name: 'Edit', 
-                    callback: function(key,opt){
-                        var modalInstance = $uibModal.open({
-                            animation: true,
-                            size: 'md',
-                            templateUrl: 'modules/booking/views/schedulerEdit.html',
-                            controller: 'schedulerEditCtrl',
-                            resolve: {
-                                event: function(){
-                                    return event;
-                                }
-                            },
-                        });
-                        modalInstance.result
-                            .then(function(result) {
-                                var dataEvent = {
-                                    resourceId: resource.id,
-                                    title: result.service,
-                                    start: start,
-                                    end: end,
-                                    allDay: allDay,
-                                    isReoccurance: result.isReoccurance,
-                                    reoccuranceType: result.reoccuranceType,
-                                    endReoccurance: result.endReoccurance,
-                                };
-                                $scope.events.push(result);
-                                $scope.scheduler.fullCalendar('renderEvent', dataEvent, true);
-                                $scope.scheduler.fullCalendar('unselect');
-                            }, function(result) {
-                                // dismiss
+        if (event.rendering != 'background') {
+            $(element).attr('id', 'event_id_'+event._id);
+            $.contextMenu({
+                selector: '#event_id_'+event._id,
+                items: {
+                    edit: {
+                        name: 'Edit', 
+                        callback: function(key,opt){
+                            var modalInstance = $uibModal.open({
+                                animation: true,
+                                size: 'md',
+                                templateUrl: 'modules/booking/views/schedulerEdit.html',
+                                controller: 'schedulerEditCtrl',
+                                resolve: {
+                                    event: function(){
+                                        return event;
+                                    }
+                                },
                             });
+                            modalInstance.result
+                                .then(function(result) {
+                                    var dataEvent = {
+                                        resourceId: resource.id,
+                                        title: result.service,
+                                        start: start,
+                                        end: end,
+                                        allDay: allDay,
+                                        isReoccurance: result.isReoccurance,
+                                        reoccuranceType: result.reoccuranceType,
+                                        endReoccurance: result.endReoccurance,
+                                    };
+                                    $scope.events.push(result);
+                                    $scope.scheduler.fullCalendar('renderEvent', dataEvent, true);
+                                    $scope.scheduler.fullCalendar('unselect');
+                                }, function(result) {
+                                    // dismiss
+                                });
+                        },
+                        icon: function(opt, $itemElement, itemKey, item){
+                            // Set the content to the menu trigger selector and add an bootstrap icon to the item.
+                            $itemElement.html('<span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Edit');
+                            // Add the context-menu-icon-updated class to the item
+                            return 'context-menu-icon-updated';
+                        }
                     },
-                    icon: function(opt, $itemElement, itemKey, item){
-                        // Set the content to the menu trigger selector and add an bootstrap icon to the item.
-                        $itemElement.html('<span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Edit');
-                        // Add the context-menu-icon-updated class to the item
-                        return 'context-menu-icon-updated';
+                    delete: {
+                        name: 'Delete',
+                        callback: function(key, opt){
+                            var modalInstance = $uibModal.open({
+                                animation: true,
+                                size: 'md',
+                                templateUrl: 'modules/booking/views/schedulerDelete.html',
+                                controller: 'schedulerDeleteCtrl',
+                                resolve: {
+                                    event: function(){
+                                        return event;
+                                    }
+                                },
+                            });
+                            modalInstance.result
+                                .then(function(result) {
+                                    var dataEvent = {
+                                        resourceId: resource.id,
+                                        title: result.service,
+                                        start: start,
+                                        end: end,
+                                        allDay: allDay,
+                                        isReoccurance: result.isReoccurance,
+                                        reoccuranceType: result.reoccuranceType,
+                                        endReoccurance: result.endReoccurance,
+                                    };
+                                    $scope.events.push(result);
+                                    $scope.scheduler.fullCalendar('renderEvent', dataEvent, true);
+                                    $scope.scheduler.fullCalendar('unselect');
+                                }, function(result) {
+                                    // dismiss
+                                });
+                        },
+                        icon: function(opt, $itemElement, itemKey, item){
+                            // Set the content to the menu trigger selector and add an bootstrap icon to the item.
+                            $itemElement.html('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Delete');
+                            // Add the context-menu-icon-updated class to the item
+                            return 'context-menu-icon-updated';
+                        }
                     }
                 },
-                delete: {
-                    name: 'Delete',
-                    callback: function(key, opt){
-                        var modalInstance = $uibModal.open({
-                            animation: true,
-                            size: 'md',
-                            templateUrl: 'modules/booking/views/schedulerDelete.html',
-                            controller: 'schedulerDeleteCtrl',
-                            resolve: {
-                                event: function(){
-                                    return event;
-                                }
-                            },
-                        });
-                        modalInstance.result
-                            .then(function(result) {
-                                var dataEvent = {
-                                    resourceId: resource.id,
-                                    title: result.service,
-                                    start: start,
-                                    end: end,
-                                    allDay: allDay,
-                                    isReoccurance: result.isReoccurance,
-                                    reoccuranceType: result.reoccuranceType,
-                                    endReoccurance: result.endReoccurance,
-                                };
-                                $scope.events.push(result);
-                                $scope.scheduler.fullCalendar('renderEvent', dataEvent, true);
-                                $scope.scheduler.fullCalendar('unselect');
-                            }, function(result) {
-                                // dismiss
-                            });
-                    },
-                    icon: function(opt, $itemElement, itemKey, item){
-                        // Set the content to the menu trigger selector and add an bootstrap icon to the item.
-                        $itemElement.html('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Delete');
-                        // Add the context-menu-icon-updated class to the item
-                        return 'context-menu-icon-updated';
-                    }
-                }
-            },
-
-        })
+            });
+        }
     };
 
     // create new event
     $scope.select = function(start, end, jsEvent, view, resource, event, allDay ) {
-        // console.log(
-        // 	title,
-        // 	jsEvent.pageX + ',' + jsEvent.pageY,
-        // 	start.format(start, "yyyy-MM-dd HH:mm:ss"),
-        // 	end.format(end, "yyyy-MM-dd HH:mm:ss"),
-        // 	resource ? resource.id : '(no resource)'
-        // );
-
-		// var title = prompt('Event Title:');
-  //       var dataEvent = {
-	 //        resourceId: resource.id,
-	 //        title: title,
-	 //        start: start,
-  //           end: end,
-  //           allDay: allDay,
-	 //    };
-	 //    if(title){
-	 //    	$scope.events.push(event);
-	 //        $scope.scheduler.fullCalendar('renderEvent', dataEvent, true);
-	 //        $scope.scheduler.fullCalendar('unselect');
-	 //    }
-
-
-
-	    var modalInstance = $uibModal.open({
-            animation: true,
-            size: 'md',
-            templateUrl: 'modules/booking/views/schedulerCreate.html',
-            controller: 'schedulerCreateCtrl',
-            resolve: {
-                event: function(){
-                    return event;
-                }
-            },
-        });
-        modalInstance.result
+        console.log(event.rendering);
+        if (event.rendering != 'background') {
+    	    var modalInstance = $uibModal.open({
+                animation: true,
+                size: 'md',
+                templateUrl: 'modules/booking/views/schedulerCreate.html',
+                controller: 'schedulerCreateCtrl',
+                resolve: {
+                    event: function(){
+                        return event;
+                    }
+                },
+            });
+            modalInstance.result
             .then(function(result) {
                 var dataEvent = {
                     resourceId: resource.id,
@@ -224,12 +217,9 @@ app.controller('schedulerCtrl', function($scope,$timeout, $uibModal){
             }, function(result) {
                 // dismiss
             });
-
+        }
     };
-    // $scope.dayClick = function(date, jsEvent, view, resource) {
-    // };
     $scope.eventClick = function(event, jsEvent, view, start, end, element){
-        
     	// event.title = "CLICKED!";
     	// $scope.scheduler.fullCalendar('updateEvent', event);
     	// console.log('Event: ' + event.title);
@@ -237,43 +227,15 @@ app.controller('schedulerCtrl', function($scope,$timeout, $uibModal){
         // console.log('View: ' + view.name);
     };
 
-    // remove event
-    // $scope.eventRightclick = function(event, jsEvent, view) {
-    //     var r = confirm("Do you want to delete event " + event.title + " ?");
-    //     if(r == true){
-    //         for (var i = 0; i < $scope.events.length; i++){
-    //             if ($scope.events[i]._id == event._id) {
-    //                 $scope.events.splice(i,1);
-    //                 $scope.scheduler.fullCalendar('removeEvents', event._id);
-    //                 alert('Success!');
-    //                 break;
-    //             }
-    //         }  
-    //     }
-    //     // Prevent browser context menu:
-    //     return false;
-    // };
 
     
     // move a event
     $scope.eventDrop = function(event) {
         $scope.events[event.id] = event;
-
-        // console.log($scope.events[event.id].resourceId);
-        // console.log($scope.events[event.id].start.format($scope.events[event.id].start, "yyyy-MM-dd HH:mm:ss"));
-        // console.log($scope.events[event.id].end.format($scope.events[event.id].end, "yyyy-MM-dd HH:mm:ss"));
     };
     // scale a event
     $scope.eventResize = function(event, jsEvent, ui, view) {
     	$scope.events[event.id] = event;
-
-    	// console.log($scope.events[event.id].start.format($scope.events[event.id].start, "yyyy-MM-dd HH:mm:ss"));
-        // console.log($scope.events[event.id].end.format($scope.events[event.id].end, "yyyy-MM-dd HH:mm:ss"));
-
-        // console.log('eventResize', event);
-        // console.log('jsEvent', jsEvent);
-        // console.log('ui', ui);
-        // console.log('view', view);
     };
     var i = 10;
     $scope.changeEvent = function(event){

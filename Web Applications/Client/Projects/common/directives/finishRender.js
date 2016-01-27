4e5d165f01b1fc1836a70f21f04811583a946b7f
tannvdts <tannv.dts@gmail.com>
2016-01-27 10:57:09 +0700

    update menu bar

---
+++ Web Applications/Client/Projects/common/directives/finishRender.js
@@ -0,0 +1,15 @@
+angular.module('app.common.finishRender',[])
+.directive('finishRender', function() {
+        return function(scope, element, attrs) {
+            if (scope.$last)
+            {
+                $('.page-sidebar-menu li').click(function(){
+		            // alert("AAA");
+		             $('.page-sidebar-menu li').removeClass('start');
+		             $(this).addClass('start');
+		             $('.page-sidebar-menu li').removeClass('active');
+		             $(this).addClass('active');
+		        });
+            }
+        };
+    });

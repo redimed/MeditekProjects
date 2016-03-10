ab40345d1d11b823b5f7559f5cb5f832f4a48284
Giang <giangvo2511@gmail.com>
2016-03-10 14:42:11 +0700

    update model patientGP

--- Web Applications/Server/api/models/v0_1/PatientGP.js
+++ Web Applications/Server/api/models/v0_1/PatientGP.js
@@ -30,7 +30,7 @@ module.exports = {
                 }
             }
         },
-        GPFirstName: {
+        Title: {
             type: Sequelize.STRING(45),
             allowNull: true,
             validate: {
@@ -40,12 +40,190 @@ module.exports = {
                 }
             }
         },
-        GPLastName: {
+        FirstName: {
+            type: Sequelize.STRING(50),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 50],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        MiddleName: {
+            type: Sequelize.STRING(100),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 100],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        LastName: {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 255],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        Type: {
+            type: Sequelize.STRING(100),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 100],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        DOB: {
             type: Sequelize.STRING(45),
             allowNull: true,
             validate: {
                 len: {
                     args: [0, 45],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        Address1: {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 255],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        Address2: {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 255],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        Postcode: {
+            type: Sequelize.STRING(10),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 10],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        Suburb: {
+            type: Sequelize.STRING(100),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 100],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        State: {
+            type: Sequelize.STRING(100),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 100],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        CountryID: {
+            type: Sequelize.BIGINT(20),
+            allowNull: true,
+            validate: {
+                isInt: {
+                    msg: 'Must be an integer!'
+                }
+            },
+        },
+        Email: {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 255],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        HomePhoneNumber: {
+            type: Sequelize.STRING(20),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 20],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        WorkPhoneNumber: {
+            type: Sequelize.STRING(20),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 20],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        FaxNumber: {
+            type: Sequelize.STRING(20),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 20],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        Signature: {
+            type: Sequelize.BIGINT(20),
+            allowNull: true,
+            validate: {
+                isInt: {
+                    msg: 'Must be an integer!'
+                }
+            },
+        },
+        HealthLink: {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 255],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        ProviderNumber: {
+            type: Sequelize.STRING(255),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 255],
+                    msg: 'Too long!'
+                }
+            }
+        },
+        Enable: {
+            type: Sequelize.STRING(1),
+            allowNull: true,
+            validate: {
+                len: {
+                    args: [0, 1],
                     msg: 'Too long!'
                 }
             }

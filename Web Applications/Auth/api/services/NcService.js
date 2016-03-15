091ce0d7dc4564d1e36bdfefbb3bb5e984e95aad
redimed <redimed@users.noreply.github.com>
2016-03-15 17:49:11 +0700

    Revert "update code"

--- Web Applications/Auth/api/services/NcService.js
+++ Web Applications/Auth/api/services/NcService.js
@@ -1,6 +1,5 @@
-var meditek_library = require('meditek_library');
-var socketIOClient = require('socket.io-client');
-var sailsIOClient = require('sails.io.js');
-var io = sailsIOClient(socketIOClient);
-var config = sails.config.myconf;
-module.exports = meditek_library.NCI(sailsIOClient, sailsIOClient, io, config.NcUrl);
+var meditek_library=require('meditek_library');
+var socketIOClient= require('socket.io-client');
+var sailsIOClient=require('sails.io.js');
+var io=sailsIOClient(socketIOClient);
+module.exports=meditek_library.NCI(sailsIOClient,sailsIOClient,io,'http://localhost:3016');

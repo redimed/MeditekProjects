0bd8e1d940ae4f9fe6fae2b0b62b15b481103ae3
buivuong <buivuongdhmo@gmail.com>
2016-05-05 16:10:21 +0700

    Third Commit :((

--- Web Applications/EFormClient/client/working/modules/eform/eformDetail.js
+++ Web Applications/EFormClient/client/working/modules/eform/eformDetail.js
@@ -291,8 +291,6 @@ module.exports = React.createClass({
             templateUID: self.templateUID
         }
 
-        console.log(data);
-
         EFormService.createPDFForm(data)
         .then(function(response){
             var fileName = 'report_'+moment().format('X');

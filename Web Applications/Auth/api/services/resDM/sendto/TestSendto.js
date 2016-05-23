9050702a8206a1ead677f57877b1dd9dde0d9a8f
Tan Nguyen <tannv.dts@gmail.com>
2016-05-23 09:19:11 +0700

    update DM

--- Web Applications/Auth/api/services/resDM/sendto/TestSendto.js
+++ Web Applications/Auth/api/services/resDM/sendto/TestSendto.js
@@ -2,7 +2,7 @@
  * Created by tannguyen on 5/20/16.
  */
 var $q = require('q');
-module.exports = function (req) {
+module.exports = function (req, res) {
     var q = $q.defer();
     setTimeout(function() {
         q.resolve(['tan', 'trinh', 'ngoc', 'ngan']);

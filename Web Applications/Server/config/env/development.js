16710cfbdce681d724d2acada178b20bce93e73b
tannvdts <tannv.dts@gmail.com>
2016-07-14 16:15:22 +0700

    update request timeout

--- Web Applications/Server/config/env/development.js
+++ Web Applications/Server/config/env/development.js
@@ -1,5 +1,6 @@
 module.exports = {
     port: 3005,
+    hookTimeout: 200000,
     // ssl: {
     //     key: require('fs').readFileSync(__dirname + '../../ssl/star_redimed_com_au.key'),
     //     cert: require('fs').readFileSync(__dirname + '../../ssl/star_redimed_com_au.pem')

3b7ddde8a0c0c41f171f2947a27d9f0d15c9f499
Giang <giangvo2511@gmail.com>
2016-07-14 10:48:42 +0700

    create new directive load list eform by appointment

---
+++ Web Applications/EFormClient/client/working/modules/eform/eformAppointment/filter.js
@@ -0,0 +1,72 @@
+var EFormService = require('modules/eform/services');
+var InputText = require('common/inputText');
+var InputDate = require('common/inputDate');
+var Config = require('config');
+// var _ = require('lodash');
+
+module.exports = React.createClass({
+    search:{},
+    propTypes: {
+        onFilter: React.PropTypes.func,
+        onPress: React.PropTypes.func,
+        onLoad: React.PropTypes.func,
+    },
+    _onFilter: function(refs) {
+        // console.log("name ",refs.props.name);
+        // console.log("value ", refs.getValue());
+        if(!this.search[refs.props.name]) {
+            this.search[refs.props.name] = refs.getValue();
+        }
+        else {
+            if(refs.getValue() =='' || refs.getValue() == null) {
+                delete this.search[refs.props.name];
+            }
+            else {
+                this.search[refs.props.name] = refs.getValue();
+            }
+        }
+        this.props.onSearch(this.search);
+    },
+    _onPress: function(refs, e) {
+        if (e.keyCode === 13) {
+            this._onFilter(refs);
+        }
+    },
+    _clearSearch: function(callback) {
+        this.search.FromTime = '';
+        this.search.Code = null;
+        this.search.Name = null;
+        this.search.CreatedDate = '';
+        this.setValue();
+        callback();
+    },
+    setValue:function() {
+        for(var key in this.search) {
+            if(key == 'FromTime') {
+                this.refs.apt_date.setValue(this.search.FromTime);
+            }
+            if(key == 'Code') {
+                this.refs.apt_code.setValue(this.search.Code);
+            }
+            if(key == 'Name') {
+                this.refs.name.setValue(this.search.Name);
+            }
+            if(key == 'CreatedDate') {
+                this.refs.created_date.setValue(this.search.CreatedDate);
+            }
+        }
+    },
+	render: function(){
+        var self = this;
+		return (
+            <tr>
+                <td></td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td><InputText placeholder="Name" ref="name"  name="Name" onKeyPress={this._onPress.bind(this ,self.refs.name)} /></td>
+                <td><InputDate placeholder="Created Date" name="CreatedDate" ref="created_date" onChange={this._onFilter.bind(this,self.refs.created_date)} /></td>
+                <td width="1"></td>
+            </tr>
+		)
+	}
+})
---
+++ Web Applications/EFormServer/api/services/EForm/GetListEFormTemplateByAppointment.js
@@ -0,0 +1,134 @@
+var _ = require('lodash');
+var moment = require('moment');
+module.exports = function(data) {
+    var pagination = {};
+    pagination.whereClause = {Appointment: {} };
+    if(!data.ApptUID) {
+        var err = new Error('GetListEFormTemplateByAppointment.error');
+        err.pushError('appointmentUID.invalid');
+        throw err;
+    }
+    if(!_.isEmpty(data.search)){
+        // pagination.whereClause.Appointment = {};
+        pagination.whereClause.EForm = {};
+        for(var key in data.search) {
+            // if (key == 'FromTime') {
+            //     var dateActual = moment(data.search.FromTime, 'YYYY-MM-DD HH:mm:ss Z').toDate();
+            //     var dateAdded = moment(dateActual).add(1, 'day').toDate();
+            //     pagination.whereClause.Appointment.FromTime = {
+            //         // like: '%' + data.search.CreatedDate + '%'
+            //         '$gte': dateActual,
+            //         '$lt': dateAdded
+            //     }
+            // }
+            // if (key == 'Code') {
+            //     pagination.whereClause.Appointment.Code = {
+            //         like: '%' + data.search.Code + '%'
+            //     }
+            // }
+            if (key == 'Name') {
+                pagination.whereClause.EForm.Name = {
+                    like: '%' + data.search.Name + '%'
+                }
+            }
+            if (key == 'CreatedDate') {
+                var dateActual = moment(data.search.CreatedDate, 'YYYY-MM-DD HH:mm:ss Z').toDate();
+                var dateAdded = moment(dateActual).add(1, 'day').toDate();
+                pagination.whereClause.EForm.CreatedDate = {
+                    // like: '%' + data.search.CreatedDate + '%'
+                    '$gte': dateActual,
+                    '$lt': dateAdded
+                }
+            }
+        }
+    }
+    console.log(data);
+    if(!_.isEmpty(data.limit)) {
+        console.log("vao ???");
+        pagination.limit = data.limit;
+    }
+    if(!_.isEmpty(data.offset)) {
+        pagination.offset = data.offset;
+    }
+    if(!_.isEmpty(data.order)) {
+        pagination.order = [];
+        for(var key in data.order) {
+            var tempOrder = [];
+            // if(key == 'FromTime') {
+            //     tempOrder = [sequelize.models['Appointment'], key, data.order[key]];
+            //     pagination.order.push(tempOrder);
+            // }
+            // if(key == 'Code') {
+            //     tempOrder = [sequelize.models['Appointment'], key, data.order[key]];
+            //     pagination.order.push(tempOrder);
+            // }
+            if(key == 'Name') {
+                tempOrder = [key, data.order[key]];
+                pagination.order.push(tempOrder);
+            }
+            if(key == 'CreatedDate') {
+                tempOrder = [key, data.order[key]];
+                pagination.order.push(tempOrder);
+            }
+        }
+    }
+    return Appointment.findOne({
+        where:{
+            UID: data.ApptUID
+        }
+    })
+    .then(function(got_appt) {
+        if(!got_appt) {
+            var err = new Error('GetListEFormTemplateByAppointment.error');
+            err.pushError('Appointment.notFound');
+            throw err;
+        }
+        else {
+            pagination.whereClause.Appointment.UID = data.ApptUID;
+            return EForm.findAndCountAll({
+                include:[
+                    {
+                        model:Appointment,
+                        attributes:['ID','UID','Code','FromTime','ToTime','RequestDate'],
+                        where:pagination.whereClause.Appointment,
+                        required:true,
+                        include:[
+                            {
+                                model:Patient,
+                                attributes:['ID','UID','FirstName','LastName'],
+                                required:true,
+                                // where:{
+                                //     UID : data.patientUID
+                                // }
+                            },
+                        ],
+                    },
+                    {
+                        model:EFormTemplate,
+                        attributes:['ID','UID'],
+                        required:true,
+                    }
+                ],
+                where:{$and:[pagination.whereClause.EForm, {Status:'saved'}]},
+                limit:data.limit,
+                offset:data.offset,
+                subQuery:false,
+                order:pagination.order,
+            });
+        }
+    },function(err) {
+        throw err;
+    })
+    .then(function(got_list) {
+        if(got_list == null || got_list == '') {
+            var err = new Error('GetListEFormTemplateByAppointment.error');
+            err.pushError('Eform.queryError');
+            throw err;
+        }
+        else {
+            return got_list;
+        }
+    }, function(err) {
+        throw err;
+    });
+}

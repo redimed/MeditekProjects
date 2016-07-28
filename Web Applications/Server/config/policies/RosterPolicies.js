b9217c104fa538a71e2ff99c1ce72a753b271e55
thanh1101681 <thanh1101681@gmail.com>
2016-07-28 10:29:18 +0700

    modified policies for Roster - Booking

---
+++ Web Applications/Server/config/policies/RosterPolicies.js
@@ -0,0 +1,23 @@
+module.exports = {
+    'Roster/RosterController': {
+        'RequestRoster': ['isAuthenticated', 'isAdminOrAssistant'],
+        'UpdateRequestRoster': ['isAuthenticated', 'isAdminOrAssistant'],
+        'GetDetailRoster': ['isAuthenticated', 'isAdminOrAssistant'],
+        'GetListRoster': ['isAuthenticated', function(req, res, next) {
+            var isNext = false;
+            _.forEach(req.user.roles, function(role_v, role_i) {
+                if (role_v.RoleCode == 'ADMIN' ||
+                    role_v.RoleCode == 'ASSISTANT' ||
+                    role_v.RoleCode == 'EXTERTAL_PRACTITIONER' ||
+                    role_v.RoleCode == 'INTERNAL_PRACTITIONER') {
+                    isNext = true
+                }
+            });
+            if (isNext) {
+                return next();
+            }
+            return res.unauthor('GetListRoster - user must be admin, assistant or doctor');
+        }],
+        'DestroyRoster': ['isAuthenticated', 'isAdminOrAssistant']
+    }
+};

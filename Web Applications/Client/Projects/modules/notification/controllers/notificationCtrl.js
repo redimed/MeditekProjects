416cf20e7a24cf49d82596f3b21e318f0e928e61
kiettran10 <kiettran@meditekvn.com>
2016-08-03 16:28:07 +0700

    Update tab in notification list

--- Web Applications/Client/Projects/modules/notification/controllers/notificationCtrl.js
+++ Web Applications/Client/Projects/modules/notification/controllers/notificationCtrl.js
@@ -1,9 +1,6 @@
 var app = angular.module("app.authentication.notification.controller", [
     "app.authentication.notification.global.controller",
-    "app.authentication.notification.global.detail.controller",
     "app.authentication.notification.private.controller",
-    "app.authentication.notification.sended.controller",
-    "app.authentication.notification.user.list.controller",
     "app.authentication.notification.list.controller",
     "app.authentication.notification.todo.controller",
     "app.authentication.notification.request.controller",

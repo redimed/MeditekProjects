c73f43d4747893838bec32d808bc195bff4860a0
Giang <giangvo2511@gmail.com>
2016-08-24 14:21:23 +0700

    update appointment status

--- Web Applications/Client/Projects/modules/WAAppointment/js/WA_helper.js
+++ Web Applications/Client/Projects/modules/WAAppointment/js/WA_helper.js
@@ -2,31 +2,48 @@ var WAConstant = {
     apptStatus: [{
         value: 'Received',
         name: 'Received',
-        image: './theme/assets/layouts/layout/imgStatus/received.png'
+        image: './theme/assets/layouts/layout/imgStatus/received.png',
+        class:'received'
     }, {
         value: 'Pending',
         name: 'Pending',
-        image: './theme/assets/layouts/layout/imgStatus/pending.png'
+        image: './theme/assets/layouts/layout/imgStatus/pending.png',
+        class:'pendding'
     }, {
         value: 'Approved',
         name: 'Appointment Time',
-        image: './theme/assets/layouts/layout/imgStatus/time.png'
+        image: './theme/assets/layouts/layout/imgStatus/time.png',
+        class:'appointment_time'
     }, {
         value: 'Attended',
         name: 'Attended',
-        image: './theme/assets/layouts/layout/imgStatus/attended.png'
+        image: './theme/assets/layouts/layout/imgStatus/attended.png',
+        class:'attended'
     }, {
         value: 'Waitlist',
         name: 'Wait List Surgery',
-        image: './theme/assets/layouts/layout/imgStatus/waitlist.png'
+        image: './theme/assets/layouts/layout/imgStatus/waitlist.png',
+        class:'wait_list_surgery',
     }, {
         value: 'Finished',
         name: 'Finished',
-        image: './theme/assets/layouts/layout/imgStatus/finish.png'
+        image: './theme/assets/layouts/layout/imgStatus/finish.png',
+        class:'finished'
     }, {
         value: 'Cancelled',
         name: 'Cancelled',
-        image: './theme/assets/layouts/layout/imgStatus/cancelled.png'
+        image: './theme/assets/layouts/layout/imgStatus/cancelled.png',
+        class:'cancelled'
+    }, {
+        value: 'Not Attended',
+        name: 'Not Attended',
+        image: '',
+        class:'not_attended'
+    }, {
+        value: 'Will Attended',
+        name: 'Will Attended',
+        image: '',
+        class:'will_attended'
     }],
     State: [{
         'code': 'VIC',
